7144c7d04c920965be7a92ec4640efb918f9eafe
Micaella Morales <morales.micaella@gmail.com>
2022-02-15 01:15:43 -0800

    Initial Commit

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Micaella Morales on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
